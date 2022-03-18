efcd3f53d95fa101b2d2a8ed714ebee05bf5c68a
Dimitrii-Kozlov <demetra_n@front.ru>
2022-03-18 13:35:50 +0300

    configure for develop: setting allowed hosts

--- support_tool/settings.py
+++ support_tool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2jkkq@o_6r(i5y1&cl2e_vmx@5s$n8m+)1@537c6afv+i(5+*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['10.211.55.8']
 
 
 # Application definition
