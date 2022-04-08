0c3a608941c049dd6443c3f84ba4f9096d37e567
Dimitrii-Kozlov <demetra_n@front.ru>
2022-04-08 16:14:22 +0300

    adjust admin panel for Case-Comment relation

--- cases/admin.py
+++ cases/admin.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 from .models import Case, Comment, AMOSModule
 
 
+class CommentAdmin(admin.TabularInline):
+    model = Comment
+
+
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'module', 'author', 'created', 'active', 'get_emails')
     list_editable = ('module', 'active')
+    inlines = [CommentAdmin]
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'body', 'author')
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'body', 'author')
 
 
 @admin.register(AMOSModule)
